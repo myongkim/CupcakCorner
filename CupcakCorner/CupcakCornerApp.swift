fd071a9fdbc176839962e91daf08b68366bdf99d
Isaac Kim <myong328@gmail.com>
2022-05-03 13:32:51 -0700

    Initial Commit

---
+++ CupcakCorner/CupcakCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakCornerApp.swift
+//  CupcakCorner
+//
+//  Created by Isaac Kim on 5/3/22.
+//
+
+import SwiftUI
+
+@main
+struct CupcakCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
